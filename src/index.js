e66d31fe77aa26452684cd03a93c032ce8ba0996
ruva-muchenje <163204916+ruva-muchenje@users.noreply.github.com>
2025-04-25 12:29:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+function searchCity(city){
+    let apiKey = "ea85029e8b68d4f3f0a195t3ob3f527a";
+    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=$(city)&key=${apiKey}&unit=metric";
+    console.log{apiUrl};
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
+
+
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
   }
   
   let searchFormElement = document.querySelector("#search-form");

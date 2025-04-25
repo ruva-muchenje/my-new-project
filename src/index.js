67584d09fb6a8f6ef26959795da7f62bdac415d1
ruva-muchenje <163204916+ruva-muchenje@users.noreply.github.com>
2025-04-25 13:08:35 +0200

    updated js and html

--- src/index.js
+++ src/index.js
@@ -1,27 +1,58 @@
-function refreshweather(rensponse) {
-    let temperatureElement = document.querySelector{"#temperature"};
-    
-    temperatureElement.innerHTML = Math.round(temperature);
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+  
     cityElement.innerHTML = response.data.city;
-    
-}
-
-function searchCity(city){
-    let apiKey = "ea85029e8b68d4f3f0a195t3ob3f527a";
-    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=$(city)&key=${apiKey}&unit=metric";
-    axios.get(apiUrl).then(refreshweather);
-}
-
-
-function handleSearchSubmit(event) {
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  }
+  
+  function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    return `${day} ${hours}:${minutes}`;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+  }
+  
+  function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-
-
-    cityElement.innerHTML = searchInput.value;
+  
     searchCity(searchInput.value);
   }
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  searchCity("Harare");
